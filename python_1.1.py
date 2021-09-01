092a0e24eb74102d46a19d1b0a789e5eab33227d
Sergey1995wasd <89312044+Sergey1995wasd@users.noreply.github.com>
2021-09-01 16:04:00 +0300

    Add files via upload

---
+++ python_1.1.py
@@ -0,0 +1,6 @@
+duration = int(input('Число: '))
+d = duration // 86400
+h = (duration - (d * 86400)) // 3600
+m = (duration - (h * 3600)) // 60
+s = duration % 60
+print(d, 'сутки', h, 'час', m, 'мин', s, 'сек')
